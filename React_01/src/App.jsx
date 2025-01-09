47ae363b2f90251c87bddc0cb2280fd228d7ebf3
Yuvraj Singh <yuvrajkypn@gmail.com>
2025-01-10 01:23:50 +0530

    Adding React_01

---
+++ React_01/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Chai from './chai'
+
+function App() {
+
+  return (
+    <>
+    <Chai/>
+    <h1>my name</h1>
+    </>
+  )
+ 
+}
+
+export default App
---
+++ React_01/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h3>hello guys my name is yuvraj singh</h3>
+    )
+}
+
+export default Chai
