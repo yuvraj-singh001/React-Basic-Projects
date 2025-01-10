99df8c4ff62eabaee7f2dbe830a884e8ccda0d72
Yuvraj Singh <yuvrajkypn@gmail.com>
2025-01-10 13:41:12 +0530

    Adding Counter

---
+++ Basic_Counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //useState() is a hook.{coubter} is a value and {setCounter} is a function and we can also change its name. 
+  let [counter, setCounter] = useState(0)
+  // /let counter = 5;
+  
+  //For increment:
+  const addValue = () =>{
+    if(counter===20){
+      // counter = counter;
+      setCounter(counter);
+    }
+    else{
+      counter = counter+1;
+      setCounter(counter); //we can use as counter =counter+1 and use updated value.
+    }
+  }
+  
+  //For decrement:
+  const removeValue = () => {
+    if(counter===0){
+      // conuter = counter;
+      setCounter(counter);
+    }
+    else{
+      counter =counter - 1;
+      setCounter(counter);  
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add value: {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove value: {counter}</button>
+    </>
+  )
+}
+
+export default App
