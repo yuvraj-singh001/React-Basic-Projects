e81880ed110fbb299c8f83734a39691b55fa58ab
Yuvraj Singh <yuvrajkypn@gmail.com>
2025-01-17 22:25:19 +0530

    adding simple code

--- Bg_Changer/src/App.jsx
+++ Bg_Changer/src/App.jsx
@@ -12,17 +12,17 @@ function App() {
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 '>
           <div className='flex flex-wrap justify-evenly gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl w-1/2'>
 
-            <button onClick={()=>{setColor("red")}} className='outline-none px-4 rounded-full text-white shadow-lg font-bold' style={{backgroundColor: "red"}}>Red</button>
+            <button onClick={()=>{setColor("red")}} className='outline-none px-4 rounded-full text-white shadow-lg font-bold bg-red-600'>Red</button>
 
-            <button onClick={()=>{setColor("green")}} className='outline-none px-4 rounded-full text-white shadow-lg font-bold' style={{backgroundColor: "green"}}>Green</button>
+            <button onClick={()=>{setColor("green")}} className='outline-none px-4 rounded-full text-white shadow-lg font-bold bg-green-600'>Green</button>
 
-            <button onClick={()=>{setColor("blue")}} className='outline-none px-4 rounded-full text-white shadow-lg font-bold' style={{backgroundColor: "blue"}}>Blue</button>
+            <button onClick={()=>{setColor("blue")}} className='outline-none px-4 rounded-full text-white shadow-lg font-bold bg-blue-500'>Blue</button>
 
-            <button onClick={()=>{setColor("purple")}} className='outline-none px-4 rounded-full text-white shadow-lg font-bold' style={{backgroundColor: "purple"}}>Purple</button>
+            <button onClick={()=>{setColor("purple")}} className='outline-none px-4 rounded-full text-white shadow-lg font-bold bg-purple-500'>Purple</button>
 
-            <button onClick={()=>{setColor("yellow")}} className='outline-none px-4 rounded-full text-black shadow-lg font-bold' style={{backgroundColor: "yellow"}}>Yellow</button>
+            <button onClick={()=>{setColor("yellow")}} className='outline-none px-4 rounded-full text-black shadow-lg font-bold bg-yellow-400'>Yellow</button>
 
-            <button onClick={()=>{setColor("pink")}} className='outline-none px-4 rounded-full text-black shadow-lg font-bold' style={{backgroundColor: "pink"}}>Pink</button>
+            <button onClick={()=>{setColor("pink")}} className='outline-none px-4 rounded-full text-black shadow-lg font-bold bg-pink-500'>Pink</button>
 
           </div>
         </div>
