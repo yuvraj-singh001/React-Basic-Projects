7cef022be6860416b250a0f0765d80fa204c8162
Yuvraj Singh <yuvrajkypn@gmail.com>
2025-01-18 01:06:46 +0530

    debuged

--- Currency_Convertor/src/App.jsx
+++ Currency_Convertor/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const[to,setTo] = useState("inr")
   const[convertedAmount, setConvertedAmount] = useState(0)
 
+//   calling custom hook
   const currencyInfo = UseCurrencyInfo(from)
 
   const options = Object.keys(currencyInfo)
@@ -33,19 +34,14 @@ function App() {
     >
         <div className="w-full">
             <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        convert()
-                       
-                    }}
-                >
+
+
                     <div className="w-full mb-1">
                         <InputBox
                             label="From"
                             amount={amount}
                             currencyOptions = {options}
-                            onCurrencyChange={(currency)=>setAmount(amount)}
+                            onCurrencyChange={(currency)=>setFrom(currency)}
                             selectCurrency={from}
                             onAmountChange={(amount)=>setAmount(amount)}
                             
@@ -60,17 +56,27 @@ function App() {
                             swap
                         </button>
                     </div>
+
                     <div className="w-full mt-1 mb-4">
                         <InputBox
                             label="To"
                             amount={convertedAmount}
                             currencyOptions = {options}
                             onCurrencyChange={(currency)=>setTo(currency)}
-                            selectCurrency={from}
+                            selectCurrency={to}
 
                             amountDisable
                         />
                     </div>
+
+                    <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        convert()
+                       
+                    }}
+                >
+
                     <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
                         Convert {from.toUpperCase() } to {to.toUpperCase()}
                     </button>
